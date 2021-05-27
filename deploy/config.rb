faa9570cf65a1cbe67145b98fdaf9fab7c62758a
Jasser Garcia Cervantes <jasser.garcia@scotiabankcolpatria.com>
2021-05-27 10:55:57 -0500

    added

--- deploy/config.rb
+++ deploy/config.rb
@@ -1,20 +1,22 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-PROJECT = 'cae-api'
+PROJECT = 'skibutlers.com-admin-light'
 
 GIT_BRANCH = `git branch --show-current 2> /dev/null`.strip
 GIT_COMMIT_SHA = `git rev-parse --verify HEAD 2> /dev/null`.strip
 GIT_TAG = `git describe --tags 2> /dev/null`.strip
 
 CONTAINER_REGISTRY_NAME = {
-  dev: 'CraftCaeDev',
+  #dev: 'CraftCaeDev',
+  dev: 'jasseralg',
   uat: 'CraftCaeUAT'
 }.freeze
 
 def container_registry
   # Here we add the registry , I need to improve this for be generic  
-  "#{CONTAINER_REGISTRY_NAME[environment.to_sym]}.azurecr.io".downcase
+  # "#{CONTAINER_REGISTRY_NAME[environment.to_sym]}.azurecr.io".downcase
+  "#{CONTAINER_REGISTRY_NAME}".downcase
 end
 
 def container_repository
